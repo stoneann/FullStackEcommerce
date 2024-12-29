0a859547697378fbca5bdf6c75b7ea95442e716f
Ann Stone <ann@thestones.me>
2024-12-29 13:06:21 -0600

    install gluestack

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -1,9 +1,25 @@
 import {Slot, Stack} from "expo-router";
+import {GluestackUIProvider} from "@/components/ui/gluestack-ui-provider";
+import {StyleSheet, View} from "react-native";
+import "@/global.css";
 
 // Rendered first then the index.tsx
 // Global Providers are rendered here
 export default function RootLayout() {
     return (
-      <Stack />
+        <GluestackUIProvider mode="light">
+            <View style={styles.container}>
+                <Stack />
+            </View>
+        </GluestackUIProvider>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
